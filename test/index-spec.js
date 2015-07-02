091b9f3b4d9d98c008019ca40d5f59ada43d2115
Sp3c1 <spectator.networx@gmail.com>
2015-07-02 14:34:38 +0200

    feat(sortCSV): add a function that only accepts strings.
      - add Fixtures.

---
+++ test/index-spec.js
@@ -0,0 +1,33 @@
+// sorting array's
+//  - function should take a String
+//    - if it is not a String throw error
+//  - function should split the String in an array for each line
+//  - function should split the array's of each line at the `,`
+//  - function should sort on first an on second field of the deepest array.
+//  - result should look be a sorted array
+import assert from 'assert'
+import fs from 'fs'
+import sinon from 'sinon'
+
+assert.calledWith = sinon.calledWith;
+
+describe('Default', () => {
+  it('should not fail', () => {
+    assert.equal(1,1);
+  });
+});
+
+describe('Sort CSV', () => {
+  let fixture = fs.readFileSync('fixture/csv-list.csv').toString();
+  it('should accept only a string as a parameter', () => {
+    assert.throws(sortCSV, TypeError);
+  });
+});
+
+function sortCSV(csv) {
+  if (typeof csv != 'string') {
+    throw new TypeError();
+  }
+}
+
+class TypeError {}
